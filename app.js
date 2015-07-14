ec5b9013185aee8d27c0acb0ea3b8a927a768703
Nikolaibibo <nbockholt@fork.de>
2015-07-14 21:41:45 +0200

    socket

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var io = require('socket.io')(http);
 var spawn = require('child_process').spawn;
 
 var app = express();
@@ -53,9 +54,61 @@ app.get('/start', function (req, res, next) {
       */
 });
 
-function sayHello () {
-  console.log("saying hello");
+// IO
+var sockets = {};
+
+io.on('connection', function(socket) {
+
+  sockets[socket.id] = socket;
+  console.log("Total clients connected : ", Object.keys(sockets).length);
+
+  socket.on('disconnect', function() {
+    delete sockets[socket.id];
+
+    // no more sockets, kill the stream
+    if (Object.keys(sockets).length == 0) {
+      app.set('watchingFile', false);
+      if (proc) proc.kill();
+      fs.unwatchFile('./public/images/cam.jpg');
+
+    }
+  });
+
+  socket.on('start-stream', function() {
+    startStreaming(io);
+  });
+});
+
+function stopStreaming() {
+  if (Object.keys(sockets).length == 0) {
+    app.set('watchingFile', false);
+    if (proc) proc.kill();
+    fs.unwatchFile('./stream/image_stream.jpg');
+  }
 }
+
+function startStreaming(io) {
+
+  if (app.get('watchingFile')) {
+    io.sockets.emit('liveStream', 'cam.jpg?_t=' + (Math.random() * 100000));
+    return;
+  }
+
+  var args = ["-w", "640", "-h", "480", "-o", "./public/images/cam.jpg", "-t", "999999999", "-tl", "1000"];
+  proc = spawn('raspistill', args);
+
+  console.log('Watching for changes...');
+
+  app.set('watchingFile', true);
+
+  fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
+    io.sockets.emit('liveStream', 'cam.jpg?_t=' + (Math.random() * 100000));
+  })
+
+}
+
+
+
 
 // ERROR HANDLING
 app.use(function(req, res, next) {
