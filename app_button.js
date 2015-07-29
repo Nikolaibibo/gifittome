9cafb416c973fbf690fa59483df173bf7f50bab9
Nikolaibibo <nbockholt@fork.de>
2015-07-29 15:34:45 +0200

    asd

--- app_button.js
+++ app_button.js
@@ -41,7 +41,7 @@ var shell_string_stillimage = "raspistill -o " + target_file_still + " -w 320 -h
 var shell_string_delete = "rm -r -f /home/pi/nodejs/gifittome/public/videos/*";
 var shell_string_create_video = "raspivid -o " + target_file_h264 + " -w 320 -h 240 -t 5000 --framerate 15";
 var shell_string_convert_video = "MP4Box -fps 15 -add " + target_file_h264 + " " + target_file_mp4;
-var shell_string_ffmpeg_palette = "ffmpeg -i " + target_file_mp4 + " -vf 'fps=15,scale=320:-1:flags=lanczos,palettegen' -y " + target_file_palette;
+var shell_string_ffmpeg_palette = "ffmpeg -i " + target_file_h264 + " -vf 'fps=15,scale=320:-1:flags=lanczos,palettegen' -y " + target_file_palette;
 //var shell_string_ffmpeg_gif = "ffmpeg -i " + target_file_mp4 + " -i " + target_file_palette + " -lavfi 'fps=15,scale=320:-1:flags=lanczos [x]; [x][1:v] paletteuse' -y " + target_file_gif;
 
 // express.js PUBLIC STATIC FILES
